c16eafdf0910d0859a441246d447795adc6f8971
Pedro Paulo Brauna Calazans <pedropaulo91.calazans@gmail.com>
2024-01-25 19:11:00 -0300

    feat: Add rotas de "delete e update"

--- src/routes/logins.ts
+++ src/routes/logins.ts
@@ -40,4 +40,40 @@ export async function loginRoutes(app: FastifyInstance) {
 
     return response.status(201).send("Sucesso");
   });
+
+  app.put("/:id", async (request, response) => {
+    const updateLoginSchema = z.object({
+      title: z.string(),
+      user: z.string(),
+      pass: z.string(),
+    });
+
+    const verificarIdLogin = z.object({
+      id: z.string().uuid(),
+    });
+
+    const { id } = verificarIdLogin.parse(request.params);
+
+    const { title, user, pass } = updateLoginSchema.parse(request.body);
+
+    await knex("logins").where("id", id).update({
+      title,
+      user,
+      pass,
+    });
+
+    return response.status(200).send("Editado com sucesso");
+  });
+
+  app.delete("/:id", async (request, response) => {
+    const deleteLoginParamsSchema = z.object({
+      id: z.string().uuid(),
+    });
+
+    const { id } = deleteLoginParamsSchema.parse(request.params);
+
+    await knex("logins").where("id", id).delete();
+
+    return response.status(200).send("Excluido com Sucesso");
+  });
 }
